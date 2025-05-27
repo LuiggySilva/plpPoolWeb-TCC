a26df10e1676c26e6216c5cecb7d53546c3c5b05
Luiggy Silva <luiggy.silva@ccc.ufcg.edu.br>
2025-05-27 15:40:40 -0300

    Reset do projeto: nova versão iniciada do zero

--- plpPool/templatetags/str_lines.py
+++
@@ -1,9 +0,0 @@
-from django.template.defaultfilters import register
-
-@register.filter(name='str_lines')
-def str_lines(string):
-    return string.split('\n')
-
-@register.filter(name='str_lines_strip')
-def str_lines_strip(string):
-    return string.strip().split('\n')
